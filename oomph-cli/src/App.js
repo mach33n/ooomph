d4a429939d2be6cc7e4dbd41db66573b81cd925a
Cameron Bennett <mr.macheen@gmail.com>
2020-12-16 21:49:22 -0500

    Client User routing updated

--- oomph-cli/src/App.js
+++ oomph-cli/src/App.js
@@ -5,7 +5,6 @@ import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import Button from 'react-bootstrap/Button';
 import axios from 'axios';
-
 import MapboxDirectionsFactory from '@mapbox/mapbox-sdk/services/directions';
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoic21hY2tjYW0iLCJhIjoiY2p3NWx0Z3ZoMXVldjQ4cXF6MWZrMGZ5NyJ9.EgCkRVGAAUDmUVYR-JSfeg';
@@ -13,115 +12,164 @@ axios.defaults.baseURL = "http://localhost:3000"
 
 const directionsCli = MapboxDirectionsFactory({accessToken: 'pk.eyJ1Ijoic21hY2tjYW0iLCJhIjoiY2p3NWx0Z3ZoMXVldjQ4cXF6MWZrMGZ5NyJ9.EgCkRVGAAUDmUVYR-JSfeg'})
 
+var map = null;
+var gtBounds = [-84.419267,33.768374,-84.378433,33.785086];
+
 class App extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {
       selected: false,
       notifDriv: false,
       driverName: '',
-      driverLicense: ''
+      driverLicense: '',
+      locM: null,
+      destM: null
     };
   }
 
   componentDidMount() {
-    // get current location using javascript
-    navigator.geolocation.getCurrentPosition(position => {
-      const map = new mapboxgl.Map({
+      // initialize map
+      map = new mapboxgl.Map({
         container: this.mapContainer,
         style: 'mapbox://styles/mapbox/streets-v11',
         // Bounds of gt campus
-        bounds: [-84.419267,33.768374,-84.378433,33.785086],
+        bounds: gtBounds,
         zoom: 10
       });
-
-      // add marker to current location
-      // var marker = new mapboxgl.Marker()
-      //   .setLngLat([position.coords.longitude, position.coords.latitude])
-      //   .addTo(map);
-
       var geocoder = new MapboxGeocoder({ // Initialize the geocoder
         accessToken: mapboxgl.accessToken, // Set the access token
         mapboxgl: mapboxgl, // Set the mapbox-gl instance
         marker: false, // Do not use the default marker style
-        bbox: [-84.419267,33.768374,-84.378433,33.785086],
+        bbox: gtBounds,
         placeholder: 'Search places on campus'
       });
-        // Add the geocoder to the map
-        map.addControl(geocoder);
+      // Add the geocoder to the map
+      map.addControl(geocoder);
+      this.onLocUpdate();
+      geocoder.on('result', async (dest) => {
+        this.rideReq(dest);
+      });
+  }
 
-        map.on('load', function() {
-          map.addSource('single-point', {
-            type: 'geojson',
-            data: {
-              type: 'FeatureCollection',
-              features: []
-            }
-          });
-          map.getSource('single-point').setData({
-            "type": "Feature",
-            "geometry": {
-              "type": "Point",
-              "coordinates": [position.coords.longitude, position.coords.latitude]
-            }
-          });
-        });
-        geocoder.on('result', async (e) => {
-          map.addLayer({
-            id: 'point',
-            source: 'single-point',
-            type: 'circle',
-            paint: {
-              'circle-radius': 10,
-              'circle-color': '#448ee4'
-            }
-          });
-          map.getSource('single-point').setData(e.result.geometry);
-          const dirReq = {
-            waypoints: [
-              {coordinates: [position.coords.longitude, position.coords.latitude]},
-              {coordinates: e.result.geometry.coordinates},
-            ],
-            profile: 'driving',
-            geometries: 'geojson'
-          }
-          const resp = await directionsCli.getDirections(dirReq).send()
-          console.log(resp.body.routes[0].geometry.coordinates)
-          map.addSource('route', {
-            'type': 'geojson',
-            'data': {
-              'type': 'Feature',
-              'properties': {},
-              'geometry': {
-                'type': 'LineString',
-                'coordinates': resp.body.routes[0].geometry.coordinates
-              }
-              }
-            });
-            map.addLayer({
-              'id': 'route',
-              'type': 'line',
-              'source': 'route',
-              'layout': {
-              'line-join': 'round',
-              'line-cap': 'round'
-              },
-              'paint': {
-              'line-color': '#888',
-              'line-width': 8
-              }
-              });
-            this.setState({selected:true});
-        });
-      })
+  componentDidUpdate() {
+    this.onLocUpdate();
+  }
+
+  // This function updates the marker for the user position
+  onLocUpdate = () => {
+    navigator.geolocation.getCurrentPosition(position => {
+      var user = document.createElement('user');
+      user.className = 'user';
+
+      var location = new mapboxgl.Marker(user);
+      location.setLngLat([position.coords.longitude, position.coords.latitude]);
+      location.addTo(map);
+      try {
+        this.state.locM.remove();
+      }
+      catch {
+        console.log('oop');  
+      }
+      this.setState({
+        locM: location
+      });
+      if (this.state.selected) {
+        var coord = this.state.destM.getLngLat()
+        this.rideReq({result:{geometry:{coordinates:[coord.lng,coord.lat]}}})
+      }
+    });
+  }
+
+  withinBounds(bb, coord) {
+    return bb[0] <= coord[0] && coord[0] <= bb[2] && bb[1] <= coord[1] && coord[1] <= bb[3]
+  }
+
+  // This function displays a given destination and its route.
+  rideReq = async (dest) => {
+    // put destination marker first
+    try {
+      this.state.destM.remove();
+    }
+    catch {
+      console.log('oop');  
+    }
+
+    var loc = this.state.locM.getLngLat();
+    var dst = dest.result.geometry.coordinates
+    console.log(this.withinBounds(gtBounds,dst))
+    console.log(this.withinBounds(gtBounds,[loc.lng,loc.lat]))
+    if (!this.withinBounds(gtBounds,dst) || !this.withinBounds(gtBounds,[loc.lng,loc.lat])) {
+      try {
+        map.removeLayer('route');
+        map.removeSource('route');
+      }
+      catch {
+        console.log('oop');  
+      }
+      return
+    }
+
+    var destM = new mapboxgl.Marker();
+    destM.setLngLat(dest.result.geometry.coordinates);
+    destM.addTo(map);
+    this.setState({
+      destM: destM
+    });
+
+    // assemble rough route outline
+    const dirReq = {
+      waypoints: [
+        {coordinates: [loc.lng,loc.lat]},
+        {coordinates: dest.result.geometry.coordinates},
+      ],
+      // could offer bike and walking options too
+      profile: 'driving',
+      geometries: 'geojson'
+    }
+    const resp = await directionsCli.getDirections(dirReq).send()
+
+    try {
+      map.removeLayer('route');
+      map.removeSource('route');
+    }
+    catch {
+      console.log('oop');  
+    }
+
+    map.addSource('route', {
+      'type': 'geojson',
+      'data': {
+        'type': 'Feature',
+        'properties': {},
+        'geometry': {
+          'type': 'LineString',
+          'coordinates': resp.body.routes[0].geometry.coordinates
+        }
+      }
+    });
+    map.addLayer({
+      'id': 'route',
+      'type': 'line',
+      'source': 'route',
+      'layout': {
+      'line-join': 'round',
+      'line-cap': 'round'
+      },
+      'paint': {
+      'line-color': '#5dde12',
+      'line-width': 8
+      }
+    });
+    this.setState({selected:true});
   }
 
   // This is meant to check if the user is within campus bounds and then if they are, request a driver.
   handleRideConf = () => {
     navigator.geolocation.getCurrentPosition(position => {
-      var bb = [33.76,-84.41,33.78,-84.37]
-      var p = position.coords
-      if ( bb[0] <= p.latitude && p.latitude <= bb[2] && bb[1] <= p.longitude && p.longitude <= bb[3]) {
+      var loc = this.state.locM.getLngLat();
+      if (this.withinBounds(gtBounds, loc)) {
         axios.post('/getDriver', {
           lat: position.coords.latitude,
           long: position.coords.longitude
